bae2ed4e3799f9466358f1f3a9a19c9caaedd05c
C1320761 Danilo Fernando Cavalcanti Leite <c1320761@interno.bb.com.br>
2023-06-02 16:41:30 -0300

    Refatoração Completa do codigo remoçao do Rx para usar o FLowState e o corotine. Melhoria no teste clear

--- app/src/test/java/com/dleite/carrefourdev/common/test/BaseUnitTest.kt
+++ app/src/test/java/com/dleite/carrefourdev/common/test/BaseUnitTest.kt
@@ -11,9 +11,4 @@ abstract class BaseUnitTest {
     val rule = InstantTaskExecutorRule()
 
 
-    @Throws(FileNotFoundException::class)
-    fun readFile(path: String): String {
-        val content: URL? = ClassLoader.getSystemResource(path)
-        return content?.readText() ?: throw FileNotFoundException("File path: $path was not found")
-    }
 }
